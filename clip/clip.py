e2e2fc6933430468c0ee03b2cb8dee93e5f2c050
Will <will@abelpolice.com>
2025-01-27 11:36:10 -0800

    update to only use local models

--- clip/clip.py
+++ clip/clip.py
@@ -40,36 +40,36 @@ _MODELS = {
 }
 
 
-def _download(url: str, root: str):
-    os.makedirs(root, exist_ok=True)
-    filename = os.path.basename(url)
+# def _download(url: str, root: str):
+#     os.makedirs(root, exist_ok=True)
+#     filename = os.path.basename(url)
 
-    expected_sha256 = url.split("/")[-2]
-    download_target = os.path.join(root, filename)
+#     expected_sha256 = url.split("/")[-2]
+#     download_target = os.path.join(root, filename)
 
-    if os.path.exists(download_target) and not os.path.isfile(download_target):
-        raise RuntimeError(f"{download_target} exists and is not a regular file")
+#     if os.path.exists(download_target) and not os.path.isfile(download_target):
+#         raise RuntimeError(f"{download_target} exists and is not a regular file")
 
-    if os.path.isfile(download_target):
-        if hashlib.sha256(open(download_target, "rb").read()).hexdigest() == expected_sha256:
-            return download_target
-        else:
-            warnings.warn(f"{download_target} exists, but the SHA256 checksum does not match; re-downloading the file")
+#     if os.path.isfile(download_target):
+#         if hashlib.sha256(open(download_target, "rb").read()).hexdigest() == expected_sha256:
+#             return download_target
+#         else:
+#             warnings.warn(f"{download_target} exists, but the SHA256 checksum does not match; re-downloading the file")
 
-    with urllib.request.urlopen(url) as source, open(download_target, "wb") as output:
-        with tqdm(total=int(source.info().get("Content-Length")), ncols=80, unit='iB', unit_scale=True, unit_divisor=1024) as loop:
-            while True:
-                buffer = source.read(8192)
-                if not buffer:
-                    break
+#     with urllib.request.urlopen(url) as source, open(download_target, "wb") as output:
+#         with tqdm(total=int(source.info().get("Content-Length")), ncols=80, unit='iB', unit_scale=True, unit_divisor=1024) as loop:
+#             while True:
+#                 buffer = source.read(8192)
+#                 if not buffer:
+#                     break
 
-                output.write(buffer)
-                loop.update(len(buffer))
+#                 output.write(buffer)
+#                 loop.update(len(buffer))
 
-    if hashlib.sha256(open(download_target, "rb").read()).hexdigest() != expected_sha256:
-        raise RuntimeError("Model has been downloaded but the SHA256 checksum does not not match")
+#     if hashlib.sha256(open(download_target, "rb").read()).hexdigest() != expected_sha256:
+#         raise RuntimeError("Model has been downloaded but the SHA256 checksum does not not match")
 
-    return download_target
+#     return download_target
 
 
 def _convert_image_to_rgb(image):
@@ -116,12 +116,19 @@ def load(name: str, device: Union[str, torch.device] = "cuda" if torch.cuda.is_a
     preprocess : Callable[[PIL.Image], torch.Tensor]
         A torchvision transform that converts a PIL image into a tensor that the returned model can take as its input
     """
-    if name in _MODELS:
-        model_path = _download(_MODELS[name], download_root or os.path.expanduser("~/.cache/clip"))
-    elif os.path.isfile(name):
-        model_path = name
+    # if name in _MODELS:
+    #     model_path = _download(_MODELS[name], download_root or os.path.expanduser("~/.cache/clip"))
+    # elif os.path.isfile(name):
+    if not (model_directory := os.environ.get("MODEL_DIRECTORY", "")):
+        raise RuntimeError(f"MODEL_DIRECTORY env variable is not set")
+                                         
+    if os.path.isfile(f"{model_directory}/{name}.pt"):
+        # model_path = name
+        model_path = f"{model_directory}/{name}.pt"
     else:
-        raise RuntimeError(f"Model {name} not found; available models = {available_models()}")
+        available_local_models = os.listdir(model_directory)
+        raise RuntimeError(f"Model {name} not found; locally available models = {available_local_models}. To update available models at them to the abel-dev-model-weights bucket")
+        # raise RuntimeError(f"Model {name} not found; available models = {available_models()}")
 
     with open(model_path, 'rb') as opened_file:
         try:
